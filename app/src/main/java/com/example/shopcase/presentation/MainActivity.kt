81aa0f043d4c9d5a1893b3f83a01a9484b05b8b3
Муратжан Абдухакимов <murat.a@evrika.com>
2022-05-12 11:27:38 +0600

    First join

---
+++ app/src/main/java/com/example/shopcase/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopcase.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shopcase.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
